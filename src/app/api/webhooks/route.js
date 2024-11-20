86a21f482c34d02b0e443662783b40baaaaa61e3
Geraldo <geraldoaarr@gmail.com>
2024-11-20 17:12:51 +0700

    update clerk

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -5,11 +5,11 @@ import { clerkClient } from '@clerk/nextjs/server';
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const SIGNING_SECRET = process.env.SIGNING_SECRET;
 
-  if (!WEBHOOK_SECRET) {
+  if (!SIGNING_SECRET) {
     throw new Error(
-      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+      'Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local'
     );
   }
 
@@ -31,7 +31,7 @@ export async function POST(req) {
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+  const wh = new Webhook(SIGNING_SECRET);
 
   let evt;
 
