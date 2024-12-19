d463586bd8592d7ae4aada21167930791aa7cd9a
Geraldo <geraldoaarr@gmail.com>
2024-12-19 12:16:59 +0700

    update project page and title url web

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -8,7 +8,7 @@ export default function Projects() {
       <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
         <h1 className='text-3xl font-semibold'>Projects</h1>
         <p className='text-md text-gray-500'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          
         </p>
         <CallToAction />
       </div>
